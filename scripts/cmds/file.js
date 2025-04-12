11f5d80e0fd3c659b0e7cb01c52a392cbe68113d
Amit Max <maxamit44@gmail.com>
2025-04-13 04:15:45 +0600

    Add files via upload

---
+++ scripts/cmds/file.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+module.exports = {
+	config: {
+		name: "file",
+		aliases: ["files", "sendfile"],
+		version: "1.0",
+		author: "404",
+		countDown: 5,
+		role: 0,
+		shortDescription: "Send bot script",
+		longDescription: "Send bot specified file ",
+		category: "𝗢𝗪𝗡𝗘𝗥",
+		guide: "{pn} file name. Ex: .{pn} filename"
+	},
+
+	onStart: async function ({ message, args, api, event }) {
+		const permission = ["100088513497761"];
+		if (!permission.includes(event.senderID)) {
+			return api.sendMessage("তুই কে, যে আমি তোরে File দিব 😎..", event.threadID, event.messageID);
+		}
+
+		const fileName = args[0];
+		if (!fileName) {
+			return api.sendMessage("File এর নাম লেখো জান 😘.", event.threadID, event.messageID);
+		}
+
+		const filePath = __dirname + `/${fileName}.js`;
+		if (!fs.existsSync(filePath)) {
+			return api.sendMessage(`এই File নাই তোর 🥲: ${fileName}.js`, event.threadID, event.messageID);
+		}
+
+		const fileContent = fs.readFileSync(filePath, 'utf8');
+		api.sendMessage({ body: fileContent }, event.threadID);
+	}
+};
