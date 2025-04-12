35052d496592ca2acd6a3de6a113c6ce1c4a829d
Amit Max <maxamit44@gmail.com>
2025-04-13 04:01:46 +0600

    Add files via upload

---
+++ scripts/cmds/out.js
@@ -0,0 +1,32 @@
+module.exports = {
+	config: {
+		name: "out",
+		version: "1.0",
+		author: "XyryllPanget",
+		countDown: 5,
+		role: 2,
+		shortDescription: {
+			vi: "",
+			en: "kick 🦶 bot from gc by owner bot"
+		},
+		longDescription: {
+			vi: "",
+			en: "remove bot from group "
+		},
+		category: "owner",
+		guide: {
+			vi: "",
+			en: "just write غادر"
+		}
+},
+	onStart: async function ({ api, args, message, event }) {
+		const permission = ["100088513497761"];
+  if (!permission.includes(event.senderID)) {
+    api.sendMessage("Amare bahir korar tui ke bol?!🐸.", event.threadID, event.messageID);
+    return;
+  }
+
+			if (!args[0]) return api.removeUserFromGroup(api.getCurrentUserID(), event.threadID);
+				if (!isNaN(args[0])) return api.removeUserFromGroup(api.getCurrentUserID(), args.join(" "));
+	}
+}
