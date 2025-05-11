17ee5c2e2354e4d5993ca1a02cd5d8b2be5b458a
Amit Max <maxamit44@gmail.com>
2025-05-11 11:29:28 +0600

    Update hug.js

--- scripts/cmds/hug.js
+++ scripts/cmds/hug.js
@@ -1,4 +1,4 @@
-.cmd install hug.js const fs = require("fs");
+const fs = require("fs");
 const path = require("path");
 const axios = require("axios");
 const { loadImage, createCanvas } = require("canvas");
